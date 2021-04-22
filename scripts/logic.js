c90b68740fb83bc1665763f6fbea9a82940a54fe
Bryan Sanchez <bryanipsanchez@gmail.com>
2021-04-22 07:53:14 -0700

    Got rid of old grid generation method

--- scripts/logic.js
+++ scripts/logic.js
@@ -7,50 +7,11 @@ const btn = document.querySelector("button");
 btn.addEventListener("click", resetGrid);
 
 
-function findSize(N) {
-    return Math.round(100/N);
-}
-
-function createGrid(n){
-    const size = findSize(n);
-    for (let i=0; i<n; i++){
-        let row = document.createElement("div");
-        row.classList.add("row");
-        row.style.width = "100%";
-        row.style.height = `${size}%`;
-        row.style.margin = "auto";
-        for (let j=0; j<n; j++){
-            let cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.setAttribute("data-key", i);
-            cell.style.width = `${size}%`;
-            cell.style.height = "100%";
-            cell.addEventListener("mouseover", function (){
-                if (cell.style.backgroundColor === "green"){
-                    cell.style.backgroundColor="white";
-                } else {
-                    cell.style.backgroundColor="green";
-                }
-            });
-            // cell.addEventListener("mouseout", function(){
-            //     cell.style.backgroundColor="white";
-            // })
-            console.log(cell);
-            row.appendChild(cell);
-        }
-        container.appendChild(row);
-    }
-}
-
 function randColor(){
     R = Math.random()*256;
     G = Math.random()*256;
     B = Math.random()*256;
     return `rgba(${R}, ${G}, ${B})`;
-}
-
-function clearGrid(){
-    container.innerHTML = '';
 }
 
 function newGrid(n) {
@@ -61,9 +22,13 @@ function newGrid(n) {
         cell.addEventListener("mouseover", function (){
             cell.style.backgroundColor = randColor();
     })
-        container.appendChild(cell);
-        }
+    container.appendChild(cell);
     }
+}
+
+function clearGrid(){
+    container.innerHTML = '';
+}
 
 function resetGrid(){
     let size = 0;
